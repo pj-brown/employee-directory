46ddb3d4898bb7d0ee8a20570654a87ae46f2286
Patrick Brown <pjbrown05@gmail.com>
2020-08-12 22:35:43 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Search from './Search/index';
+import EmployeeCard from './EmployeeCard';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Search />
+        <EmployeeCard />
       </header>
     </div>
   );
